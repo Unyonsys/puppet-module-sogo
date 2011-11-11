a0cc7f4c5f6681b647d25892bb01e97c7a7882dc
Bruno Léon <bruno.leon@unyonsys.com>
2011-11-11 15:25:15 +0000

    Reorganised db management

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,13 @@
 class sogo (
   $sogo_db_password,
   $sogo_workers_count = 3
-  ) {
-
-  Mysql::Database['sogo']         -> Class['sogo']
-  Mysql::User['sogo']             -> Class['sogo']
-  Mysql::Rights::Standard['sogo'] -> Class['sogo']
+) {
 
   Class["${module}::install"] -> Class["${module}::config"] ~> Class["${module}::service"]
 
-  class { "${module}::install": }
+  class { "${module}::install":
+    sogo_db_password   => $sogo_db_password
+  }
   class { "${module}::config":
     sogo_db_password   => $sogo_db_password,
     sogo_workers_count => $sogo_workers_count
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,6 +1,18 @@
-class sogo::install {
+class sogo::install (
+  $sogo_db_password
+) {
   package { 'sogo':
     ensure => present
   }
+
+  mysql::user { 'sogo':
+    user      => sogo,
+    password  => $sogo_db_password,
+  }
+  mysql::rights::standard { 'sogo':
+    database  => 'sogo',
+    user      => 'sogo',
+  }
+  mysql::database { 'sogo': }
 }
 
