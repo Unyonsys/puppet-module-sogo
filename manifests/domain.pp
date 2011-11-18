2dc37952a489ca944c7900a23192fdb929df186b
Bruno LÃ©on <bruno.leon@unyonsys.com>
2011-11-18 05:25:06 +0000

    Removed apache website management from sogo domain define

--- manifests/domain.pp
+++ manifests/domain.pp
@@ -3,8 +3,6 @@ define sogo::domain (
   $ldap_bind_pw,
   $sogo_website               = 'mail',
   $sogo_dav_website           = 'dav',
-  $sogo_website_aliases       = false,
-  $sogo_website_fqdnaliases   = false,
   $sogo_timezone              = 'Europe/Paris',
   $sogo_dst_folder_prefix     = 'INBOX/',
   $multi_domain_setup         = false
@@ -48,28 +46,5 @@ define sogo::domain (
     content => template('sogo/sogo-rewrite.conf.erb'),
     require => Package['apache2'],
     notify  => Exec['reload-apache2']
-  }
-
-  apache2::website { "${sogo_website}.${name}":
-      site_domain         => $name,
-      confname            => $sogo_website,
-      apache2_aliases     => $sogo_website_aliases,
-      apache2_fqdnaliases => $sogo_website_fqdnaliases,
-      required_modules    => ['proxy', 'proxy_http', 'headers'],
-      apache2_includes    => ["/home/sogo/SOGo_${name}.conf", "/etc/apache2/specific-includes/sogo-rewrite-${name}.conf"],
-      site_ips_ssl        => '*',
-      force_ssl           => true,
-      has_awstats         => false,
-  }
-
-  apache2::website { "${sogo_dav_website}.${name}":
-      site_domain         => $name,
-      confname            => $sogo_dav_website,
-      required_modules    => ['proxy', 'proxy_http', 'headers'],
-      apache2_includes    => ["/home/sogo/SOGo_dav_${name}.conf"],
-      site_ips_ssl        => '*',
-      force_ssl           => true,
-      has_awstats         => false,
-      monitor             => 'with_auth'
   }
 }
